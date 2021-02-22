8d183df47d4771ba8f0c55fa9fcfea22f1de89a3
Troy Wills <you@example.com>
2021-02-22 01:25:04 -0600

    local storage added

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,19 @@
 $("#run-search").on("click", function (event) {
-    // event.preventDefault();
-    alert('working');
+    event.preventDefault();
 
-// var apiKey = "39d310963c56645dbc7002332b76e141";
-// var citySearch = $("#searcedCity")
-// var apiUrl = `api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}`;
+var apiKey = "39d310963c56645dbc7002332b76e141";
+var citySearch = $("#searcedCity").val().trim();
+var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}`;
 
 $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=39d310963c56645dbc7002332b76e141",
+    url: apiUrl,
     method: "GET",
     }).then(function (response) {
         console.log(response.main.temp);
+    });
 });
-});
+
+
+
+
 
